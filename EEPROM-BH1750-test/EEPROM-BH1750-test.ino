476b358b4fa1ebd73e4233c6938c50b4dadc6f34
JOAQUÍN-PC\JOAQUÍN <joaquin@deprada.net>
2018-03-08 19:38:52 +0100

    EEPROM software test working!!!

--- EEPROM-BH1750-test/EEPROM-BH1750-test.ino
+++ EEPROM-BH1750-test/EEPROM-BH1750-test.ino
@@ -3,39 +3,38 @@
 #include <BH1750.h>
 #include <EEPROM.h>
 
-//byte eeCounter = 0;
-//byte picCounter = 0;
-/*
-struct Picture{
-  byte Index;
-  int  ShutterSpeed;
-  uint16_t lux;
-};
-*/
+//EEPROM fixed positions
+// 0 -->J
+// 1 -->P
+//10 --> pointer int eeAddress
 
+int eeAddress;
+
+//13 --> Picture counter int ActualPicture
+
+int ActualPicture;
+
+//15-100 --> empty for future settings auto exposure etc...
+//101 --> intial record
+
+// record struct structure
+// Picture sequence
+// Type (manual, a100,a600, flash etc...)
+// ShutterSpeed actual for auto reference/record
+// lux measured
  
   struct Picture  {
-  byte StructPicture;
+  int StructPicture;
   byte StructType;
   int StructSpeed;
   uint16_t StructLux;
   };
 
 
-int eeAddress;
-int ActualPicture;
-
-
-//byte value = EEPROM.read(eeCounter);
-
-//EEPROM.write (eeCounter, val
-
-// digital pin 2 has a pushbutton attached to it. Give it a name:
+// testing/simulation buttons / LED
 int pushButton = 6;
 int dumpButton = 4;
 int led = 5;
-
-// the setup routine runs once when you press reset:
 
 BH1750 lightmeter;
 
@@ -47,31 +46,44 @@ const byte luxMode = BH1750_CONTINUOUS_HIGH_RES_MODE;
 // BH1750_ONE_TIME_HIGH_RES_MODE_2
 // BH1750_ONE_TIME_LOW_RES_MODE
 
+
+
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
-  // make the pushbutton's pin an input:
+  // make the pushbuttons pin an input:
   pinMode(pushButton, INPUT);
   pinMode(dumpButton, INPUT_PULLUP);
+  
   pinMode(led,OUTPUT);
+
     lightmeter.begin(luxMode); // Inicializar BH1750
     pinMode(A4, INPUT_PULLUP);
     pinMode(A5, INPUT_PULLUP);
 
 
-EEPROM.get(1,eeAddress);  // where to write
+    char initJP [2];
+    initJP[0] = EEPROM.read(0);
+    initJP[1] = EEPROM.read(1);
 
-if (eeAddress >= 32000){
-  EEPROM.put(1,100);
-}
+    if (initJP[0] != 'S' || initJP[1] != 'X')
+        {
+  Serial.println ("Initializing....");
+  EEPROM.write(0,'S');
+  EEPROM.write(1,'X');
+  int eeAddress = 101;
+  EEPROM.put (10,eeAddress);
+  int ActualPicture= 1;
+  EEPROM.put (13,ActualPicture);
+  }
+      else 
+  {
+Serial.println ("NO NEED TO INITIALIZE");
+EEPROM.get(10,eeAddress);  // where to write
+EEPROM.get (13, ActualPicture); //the picture taken counter
+  }
 
-//byte ActualCounter = EEPROM.read(eeCounter);
-
-
-EEPROM.get (10, ActualPicture); //the picture taken counter
-
-
-
+Serial.println ("Running main code...");
 }
 
 
@@ -80,7 +92,7 @@ void loop() //******************************************************************
 
 {
 
-byte PictureType;
+byte PictureType = 0;
 // PictureType = 0 ---> MANUAL
 // PictureType = 1 ---> A100
 // PictureType = 2 ---> FLASH DONGLELESS
@@ -92,13 +104,14 @@ int ShutterSpeed = 66;
 
 uint16_t lux = lightmeter.readLightLevel(); // Reading BH1750
 /* 
+
   struct Picture  {
-  byte StructPicture;
+  int StructPicture;
+  byte StructType;
   int StructSpeed;
   uint16_t StructLux;
-  };
-*/
-Picture MyPicture ={ ActualPicture, ShutterSpeed, lux};
+  };*/
+Picture MyPicture ={ ActualPicture, PictureType, ShutterSpeed, lux};
   if (digitalRead(pushButton) == LOW) 
   {
   
@@ -129,6 +142,9 @@ Picture MyPicture ={ ActualPicture, ShutterSpeed, lux};
   EEPROM.get( eeAddress, MyPicture );  
   Serial.print( " Picture: " );
   Serial.println( MyPicture.StructPicture );
+  Serial.print( " Type: " );
+  Serial.println( MyPicture.StructType );
+
   Serial.print( " ShutterSpeed: " );
 
   Serial.println( MyPicture.StructSpeed );
@@ -151,11 +167,11 @@ Serial.println("****************************************************************
   if(eeAddress+sizeof(MyPicture) >= EEPROM.length()) eeAddress = 0;  //Comprobar que no hay desbordamiento
 
  
- EEPROM.update (1,eeAddress);
+ EEPROM.update (10,eeAddress);
 
  ActualPicture = ActualPicture+1;
 
- EEPROM.update (10,ActualPicture);
+ EEPROM.update (13,ActualPicture);
 
 delay (500);
   }
@@ -164,7 +180,7 @@ delay (500);
   if (digitalRead(dumpButton) == LOW) 
   {
 
-EEPROM.get(1,eeAddress);
+EEPROM.get(10,eeAddress);
 int ReadAddress = (eeAddress - (sizeof(MyPicture)*8));
 
 Serial.print("======================= Entering loop =======================");
@@ -187,8 +203,27 @@ for (int i = 0; i < 8; i++)
   Serial.println (sequence);
   Serial.print( " Picture: " );
   Serial.println( MyPicture.StructPicture );
-  Serial.print( " ShutterSpeed: " );
+  Serial.print( " Type raw: " );
+  Serial.println( MyPicture.StructType );
+  // PictureType = 0 ---> MANUAL
+// PictureType = 1 ---> A100
+// PictureType = 2 ---> FLASH DONGLELESS
+// PictureType = 4 ---> FLASH F8 DONGLE 
+// PictureType = 6 ---> A600
 
+  Serial.print( " Type: " );
+  if (MyPicture.StructType == 0){
+  Serial.println( "MANUAL" );  }  
+  if (MyPicture.StructType == 1){
+  Serial.println( "AUTO 100" ) ; }
+  if (MyPicture.StructType == 2){
+  Serial.println( "FLASH DONGLELESS" );}
+  if (MyPicture.StructType == 4){
+  Serial.println( "FLASH F8 DONGLE" ); }
+  if (MyPicture.StructType == 6){
+  Serial.println( "AUTO 600" );  }
+  
+  Serial.print( " ShutterSpeed: " );
   Serial.println( MyPicture.StructSpeed );
   Serial.print( " Lux: " );
 
