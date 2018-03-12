75e790fcd5ac5ff73fff02b8c1bf0971da161889
Joaquin de Prada <joaquin@deprada.net>
2018-03-12 16:23:47 +0100

    Cleaning up the EEPROM mess!!!

--- Arduino-WORKING/1_openSX70_WORKING-EEPROM/1_openSX70_WORKING-EEPROM.ino
+++ Arduino-WORKING/1_openSX70_WORKING-EEPROM/1_openSX70_WORKING-EEPROM.ino
@@ -21,3 +21,27 @@
               // S8: HIGH = CLOSED
               // S9: HIGH = CLOSED
 
+              //EEPROM CHEATSHEET
+              //  0-> S  initialization purposes
+              //  1-> X  initialization purppses
+              //  3-> ActualPicture --> From the beggining absolute picture counter
+              //  4-> CurrentPicture --> 1-8 "Pack" picture counter
+              // 10-> eeAddress Where to write next
+              // 13-> ActualPicture --> Counter from begining
+              //ActualPicture
+              //CurrentPicture 
+
+              // Picture:
+              //           int StructPicture--> continous picture counter  
+              //           byte PackPicture --> within pack 1-8 counter 
+              //           byte StructType  --> Type of picture (see table)
+
+                                            // PictureType = 0 ---> MANUAL
+                                            // PictureType = 1 ---> A100
+                                            // PictureType = 2 ---> FLASH DONGLELESS
+                                            // PictureType = 4 ---> FLASH F8 DONGLE 
+                                            // PictureType = 6 ---> A600
+              
+              //           int StructSpeed  --> ShutterSpeed
+              //           uint16_t StructLux --> Lux as read by BH170 just before picture-taking
+               
