07b3f6759c47749d916063e4a6e69f2c3f1dfa16
JOAQUÍN-PC\JOAQUÍN <joaquin@deprada.net>
2018-06-18 23:04:09 +0200

    test folder in Arduino

---
+++ tests/BH1750test.ino/BH1750test.ino.ino
@@ -0,0 +1,31 @@
+
+//Light meter software without pullup resistors using internal pullups.
+#include <Wire.h>
+#include <BH1750.h>
+
+BH1750 lightmeter;
+
+const byte luxMode = BH1750_CONTINUOUS_HIGH_RES_MODE;
+// BH1750_CONTINUOUS_HIGH_RES_MODE
+// BH1750_CONTINUOUS_HIGH_RES_MODE_2
+// BH1750_CONTINUOUS_LOW_RES_MODE
+// BH1750_ONE_TIME_HIGH_RES_MODE
+// BH1750_ONE_TIME_HIGH_RES_MODE_2
+// BH1750_ONE_TIME_LOW_RES_MODE
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("Starting sensor..."));
+  lightmeter.begin(luxMode); // Inicializar BH1750
+    pinMode(A4, INPUT_PULLUP);
+    pinMode(A5, INPUT_PULLUP);
+}
+
+void loop() {
+  uint16_t lux = lightmeter.readLightLevel(); // Reading BH1750
+  Serial.print(F("Lux:  "));
+  Serial.print(lux);
+  Serial.println(" lx");
+  delay(500);
+}
+
