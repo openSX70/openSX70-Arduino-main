5a680444ce5ce19bf45cc36db5da442af74c47d5
Joaquin de Prada <joaquin@deprada.net>
2018-12-07 00:32:17 +0100

    new software and tests

--- tests/photodiode-test11/photodiode-test11.ino
+++ tests/photodiode-test11/photodiode-test11.ino
@@ -1,4 +1,5 @@
 int sensorValue;
+int lightMeter = A3;
 void setup() {
   // initialize serial communication at 57600 bits per second:
   Serial.begin(57600);
@@ -30,60 +31,24 @@ digitalWrite (A4, LOW);
  
 return ;
 }
-/*
-void R14()
-{
-//Connect xx DO NOT USE!!!
-//Disconnect rest
-//R14 on the PCB
-pinMode (A4, INPUT);
-pinMode (A5, INPUT);
-pinMode (A7, OUTPUT);  // this is the one "connected"
-digitalWrite (A7, LOW);
- 
-return ;
-}*/
 
 void loop() {
   // read sensor and print values
- 
-/*
-R14();
-delay (10);
-sensorValue = analogRead(A3);
-sensorValue = analogRead(A3);
-sensorValue = analogRead(A3);
-if (sensorValue == 0)
-{
-Serial.print ("  10K: null");
-} 
-else if (sensorValue == 1023) 
-{
-Serial.print ("  10K: max");
-}
-else 
-{
-Serial.print ("  10K: ");
-Serial.print(sensorValue);
-delay (500);
-}
-*/
+
 R11();
 delay (10);
-//sensorValue = analogRead(A3);
-//sensorValue = analogRead(A3);
-sensorValue = analogRead(A3);
+sensorValue = analogRead(lightMeter);
 if (sensorValue == 0)
 {
-Serial.print ("          HIGH LIGHT 10K: too low");
+Serial.print ("LOW LIGHTS 1M5: too low");
 } 
 else if (sensorValue == 1023) 
 {
-Serial.print ("          HIGH LIGHT 10K: too much");
+Serial.print ("LOW LIGHTS 1M5: too much");
 }
 else 
 {
-Serial.print ("          HIGH LIGHT 10K: ");
+Serial.print ("LOW LIGHTS 1M5: ");
 Serial.print(sensorValue);
 delay (500);
 }
@@ -91,24 +56,24 @@ delay (500);
  
 R15();
 delay (10);
-sensorValue = analogRead(A3);
+sensorValue = analogRead(lightMeter);
 //sensorValue = analogRead(A3);
 //sensorValue = analogRead(A3);
 if (sensorValue == 0)
 {
-Serial.println ("          LOW LIGHT 1M: too low");
-Serial.println("--------------------------------------------------------");
+Serial.println ("   HIGH LIGHTS 750K: too low");
+Serial.println("---------------------------------------------------");
 } 
 else if (sensorValue == 1023) 
 {
-Serial.println ("          LOW LIGHT 1M: too much");
-Serial.println("--------------------------------------------------------");
+Serial.println ("   HIGH LIGHTS 750K: too much");
+Serial.println("---------------------------------------------------");
 }
 else 
 {
-Serial.print ("          LOW LIGHT 1M: ");
+Serial.print ("   HIGH LIGHTS 750K: ");
 Serial.println (sensorValue);
-Serial.println("--------------------------------------------------------");
+Serial.println("---------------------------------------------------");
 delay (500);
 }
 
