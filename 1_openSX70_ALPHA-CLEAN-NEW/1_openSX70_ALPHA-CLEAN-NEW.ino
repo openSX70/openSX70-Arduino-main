fce1c0172165b60cdfd18cfa4e1dd0d179f5fc8f
Joaquin De Prada on Macbook air <joaquin@deprada.net>
2019-01-19 14:13:28 +0100

    compiles on MAC

--- 1_openSX70_ALPHA-CLEAN-NEW/1_openSX70_ALPHA-CLEAN-NEW.ino
+++ 1_openSX70_ALPHA-CLEAN-NEW/1_openSX70_ALPHA-CLEAN-NEW.ino
@@ -10,7 +10,7 @@
 //this is the first "proper" GitHub code
 // 16/11/2017 Fixed many things. Optional MOTOR or SHUTTER for tests only shutter and only MOTOR. BUG: with double exposure with Switch 1 = 0 (Read_DS2408_PIO(1) = 1 (ON). For some reason does two pictures in case 2.
 // 10/03/2018 First working camera?
-// 23/03/2018 EEPROM working, eepromUpdate function, pos B and pos T working. 
+// 23/03/2018 EEPROM working, eepromUpdate function, pos B and pos T working.
 // 27/03/2018 Prototype# Len's and Bellows flash! Newer speeds! Testing BH1750 BH1750_CONTINUOUS_LOW_RES_MODE.
 // 28/03/2018 Ordered boards today!!! fixed timer led on dongle.
 //  3/07/2018 Fix for non-blinking LED!!! (thanks Pitsie!)
@@ -19,44 +19,44 @@
 
 //  7/12/2018 ALPHA
 
-              // FIRST I ASSIGN THE Sx SWITCHES OF THE SX70 TO PINS ON THE ARDUINO
-              // THESE ARE INPUTS
-              // SX70-ARDUINO LOGICAL EQUIVALENTS
-              // S1: LOW = CLOSED
-              // S3: LOW = CLOSED
-              // S5: LOW = CLOSED
-              // S8: HIGH = CLOSED
-              // S9: HIGH = CLOSED
+// FIRST I ASSIGN THE Sx SWITCHES OF THE SX70 TO PINS ON THE ARDUINO
+// THESE ARE INPUTS
+// SX70-ARDUINO LOGICAL EQUIVALENTS
+// S1: LOW = CLOSED
+// S3: LOW = CLOSED
+// S5: LOW = CLOSED
+// S8: HIGH = CLOSED
+// S9: HIGH = CLOSED
 
-              //EEPROM CHEATSHEET
-              //  0-> S  initialization purposes
-              //  1-> X  initialization purppses
-              //  3-> ActualPicture --> From the beggining absolute picture counter
-              //  4-> CurrentPicture --> 1-8 "Pack" picture counter
-              // 10-> eeAddress Where to write next
-              // 13-> ActualPicture --> Counter from begining
-              //ActualPicture
-              //CurrentPicture 
+//EEPROM CHEATSHEET
+//  0-> S  initialization purposes
+//  1-> X  initialization purppses
+//  3-> ActualPicture --> From the beggining absolute picture counter
+//  4-> CurrentPicture --> 1-8 "Pack" picture counter
+// 10-> eeAddress Where to write next
+// 13-> ActualPicture --> Counter from begining
+//ActualPicture
+//CurrentPicture
 
-              // Picture:MyPicture
-              //           int StructPicture:ActualPicture --> continous picture counter  
-              //           byte PackPicture:CurrentPicture --> within pack 1-8 counter 
-              //           byte StructType:PictureType  --> Type of picture (see table)
+// Picture:MyPicture
+//           int StructPicture:ActualPicture --> continous picture counter
+//           byte PackPicture:CurrentPicture --> within pack 1-8 counter
+//           byte StructType:PictureType  --> Type of picture (see table)
 
-                                            // PictureType = 0 ---> MANUAL
-                                            // PictureType = 1 ---> A100
-                                            // PictureType = 2 ---> FLASH DONGLELESS
-                                            // PictureType = 4 ---> FLASH F8 DONGLE 
-                                            // PictureType = 5 ---> FLASH DONGLE 
-                                            // PictureType = 6 ---> A600
-                                            // PictureType = 7 ---> Bulb
-                                            // PictureType = 8 ---> T
-                                            
-              
-              //           int StructSpeed:ShutterSpeed  --> ShutterSpeed
+// PictureType = 0 ---> MANUAL
+// PictureType = 1 ---> A100
+// PictureType = 2 ---> FLASH DONGLELESS
+// PictureType = 4 ---> FLASH F8 DONGLE
+// PictureType = 5 ---> FLASH DONGLE
+// PictureType = 6 ---> A600
+// PictureType = 7 ---> Bulb
+// PictureType = 8 ---> T
+
+
+//           int StructSpeed:ShutterSpeed  --> ShutterSpeed
 
 
 //  23/12/2018 MAJOR CLEAN UP!!!!!!!!!!!!!!!!!!! (openSX70_ALPHA-CLEAN-NEW)
 
-                 
+
 
