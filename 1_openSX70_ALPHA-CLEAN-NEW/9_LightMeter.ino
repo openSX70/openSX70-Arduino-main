3257cf48a74f9d5b3421635b8edaf02e1551ef92
Joaquin De Prada on Macbook air <joaquin@deprada.net>
2019-01-26 01:18:34 +0100

    LM

---
+++ 1_openSX70_ALPHA-CLEAN-NEW/9_LightMeter.ino
@@ -0,0 +1,65 @@
+//PJ this are my LM functions
+#if LIGHTMETER
+void R11()
+{
+//This is for LOW light condition
+//Connect 1M5
+//Disconnect rest
+//R11 on the PCB
+//pinMode (A7, INPUT);
+pinMode (A4, INPUT);
+pinMode (A5, OUTPUT);  // this is the one "connected"
+digitalWrite (A5, LOW);
+return ;
+}
+
+void R15()
+{
+  //This is for HIGH light condition
+//Connect 750K
+//Disconnect rest
+//R15 on the PCB
+//pinMode (A7, INPUT);
+pinMode (A5, INPUT);
+pinMode (A4, OUTPUT);  // this is the one "connected"
+digitalWrite (A4, LOW);
+return ;
+}
+int LightValueLOW()
+{
+R11();
+//
+delay (10);
+int sensorValue = analogRead(lightMeter);
+return sensorValue;
+}
+int LightValueHIGH()
+{
+R15();
+//
+delay (10);
+int sensorValue = analogRead(lightMeter);
+return sensorValue;
+}
+#endif
+
+int LightValueLOW2()
+//PJ this is my last test with function all-in-one
+{
+//This is for LOW light condition
+//Connect 1M5
+//Disconnect rest
+//R11 on the PCB
+//pinMode (A7, INPUT);
+pinMode (A4, INPUT);
+pinMode (A5, OUTPUT);  // this is the one "connected"
+digitalWrite (A5, LOW);
+//
+delay (10);
+// int sensorValue = analogRead(lightMeter);
+int LightValueLOW2 = analogRead(lightMeter);
+//return sensorValue;
+return LightValueLOW2;
+}
+
+
