7db74cae5eb0f9f0bfa71a5258ed56abcdf01a18
Zane Pollard <zane.pollard@thesitecontroller.local>
2020-10-23 12:43:43 -0500

    removed obsolete origami code

--- WORKING/opensx70_lib/settings.cpp
+++ WORKING/opensx70_lib/settings.cpp
@@ -19,11 +19,8 @@ byte lightmeterHelper = true;
 //int ShutterSpeed[] = { 3, 5, 9, 11, 20, 30, 35, 55, 166, 302, 600, 1100, POST, POSB, AUTO600, AUTO100 };  //SANTI DONGLE WHEEL
 
 // Added to remove the need to check for selector values prior to picture taking.
-#if UDONGLE
-  int ShutterSpeed[] = {   16,   20,   23,    25,  30,   35,     55,   55,    166,  302, 600, 1100, POST, POSB, AUTO600, AUTO100 };
-#elif ORIGAMIV1
-  int ShutterSpeed[] = { 3, 5, 9, 11, 20, 30, 35, 55, 166, 302, 600, 1100, POST, POSB, AUTO600, AUTO100 };
-#endif
+int ShutterSpeed[] = {   16,   20,   23,    25,  30,   35,     55,   55,    166,  302, 600, 1100, POST, POSB, AUTO600, AUTO100 };
+
 
 //OPTION line above are the wheel "raw" speeds (have to keep in mind smaller time = smaller aperture)
 // this are the "SLOTS":+
