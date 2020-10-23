4615e223d085c973c0d957a6b860fa6edfe123a1
Zane Pollard <pollardzane@gmail.com>
2020-10-23 13:49:01 -0500

    set up for sonar

--- WORKING/opensx70_lib/settings.h
+++ WORKING/opensx70_lib/settings.h
@@ -7,7 +7,7 @@
   #define DEBUG 1
   #define SIMPLEDEBUG 0//Simple Debug On 1 - Off 0
   #define ADVANCEDEBUG 0 //Simple Debug On 1 - Off 0
-  #define BASICDEBUG 0 // Debug of Basic Camera Functions
+  #define BASICDEBUG 1 // Debug of Basic Camera Functions
   #define MXDEBUG 0 // Multiple Exposure Debug
   #define LMDEBUG 0 //Lightmeter Debug
   #define LMHELPERDEBUG 0 //Lightmeter Debug
@@ -20,8 +20,8 @@
   #define TCS3200
   //S1Logic LOW = Alphas, Meroë, Edwin
   //#define S1Logic HIGH //= Sonar 
-  #define S1Logic LOW
-  #define SONAR 0
+  #define S1Logic HIGH
+  #define SONAR 1
   #define ORIGAMI 0
   #define ORIGAMIV1 0 //V1 Version of Origami with inverted Rotaryswitch
   #define UDONGLE 1
