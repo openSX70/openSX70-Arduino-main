26a068206f4b23567fea63bbe1dc8f80ab6e64d2
zanepollard <pollardzane@gmail.com>
2020-10-29 08:48:59 -0500

    Added TSL237T support for autoExposeFF mode

--- WORKING/opensx70_lib/TSL237T.h
+++ WORKING/opensx70_lib/TSL237T.h
@@ -12,7 +12,16 @@
   #define OPENSX70_TSL237T_H
   #define A100 950 //Magicnumber SX70
   #define A600 420 //Magicnumber 600
-  #define A400 600 //Magicnumber 600BW
+
+  #define A100 950
+  #define A600 420
+
+  //Numbers for auto FF
+  //FD 33% of MN, FE 43% of MN
+  #define FD100 317
+  #define FD600 140
+  #define FE100 409
+  #define FE600 181
   
   /* These functions are called by the main loop.
    * They provide uniform handling for whatever light sensor is used on a specific board.
