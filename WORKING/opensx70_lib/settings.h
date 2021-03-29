b04246956f661a494f8aa28ba7cea3dbd9e23fe0
zanepollard <pollardzane@gmail.com>
2021-03-29 16:27:14 -0500

    forgot to keep eject after depress when merging.

--- WORKING/opensx70_lib/settings.h
+++ WORKING/opensx70_lib/settings.h
@@ -45,6 +45,7 @@
   #define TIMER_MIRROR_UP 1    //1 Enables mirror up feature for self timer modes
   #define EIGHT_SHOT_PACK 1    //1 Makes all counter-based functions work based on an 8 shot pack rather than 10
   #define LIGHMETER_HELPER 1   //1 Enables viewfinder light meter helper
+  #define EJECT_AFTER_DEPRESSING 0 // Ejects the film only after releasing the shutter button. This gives time to the photographer to shield the film from light.
   //----------------END CAMERA PCB OPTIONS SELECTION------------------------
 
   //----------------ISO VALUES VALUES---------------------------------------
