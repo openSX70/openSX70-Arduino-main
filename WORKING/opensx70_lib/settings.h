f3ecab865bc37f7ee3b11f7c571ec993bc53c7b3
zanepollard <pollardzane@gmail.com>
2021-07-22 11:36:04 -0500

    restoring new settings.h layout

--- WORKING/opensx70_lib/settings.h
+++ WORKING/opensx70_lib/settings.h
@@ -1,37 +1,52 @@
 #ifndef settings_h
   #define settings_h
   #include "Arduino.h"
-  //------------DEBUG OPTIONS---------------------------------------------
-  #define DEBUG 1
-  #define SIMPLEDEBUG 1     //Debug of opensx70 functions
-  #define ADVANCEDEBUG 0    //Simple Debug On 1 - Off 0
-  #define BASICDEBUG 1      // Debug of Basic Camera Functions
-  #define MXDEBUG 0         // Multiple Exposure Debug
+  //----------------DEBUG OPTIONS----------------------------------------
+  #define DEBUG 0
+  #define SIMPLEDEBUG 0     //Debug of opensx70 functions
+  #define ADVANCEDEBUG 0    //More detailed view of OpenSX70 functions
+  #define BASICDEBUG 0      //Debug of Basic Camera Functions
+  #define MXDEBUG 0         //Multiple Exposure Debug
   #define LMDEBUG 0         //Lightmeter Debug
   #define LMHELPERDEBUG 0   //Lightmeter Debug
   #define ROTARYDEBUG 0     //Rotaryswitch on Dongle Debug
   #define STATEDEBUG 0      //Debug state transitions
   #define FOCUSDEBUG 0      //Focus Debug on Sonar
   #define FFDEBUG 0         //AutoexposureFillFlash Debug
-  //----------------------END DEBUG OPTIONS------------------------------
-  //----------------------CAMERA PCB OPTIONS SELECTION-------------------
-  #define TCS3200         //TSL237T TSL235R TCS3200
-  #define S1Logic LOW  //HIGH for sonar, LOW for Alpha
-  #define ALPHA 1        //1 if ALPHA camera/PCB
-  #define SONAR 0         //   
+  //----------------END DEBUG OPTIONS------------------------------------
+
+  //----------------CAMERA PCB OPTIONS SELECTION-------------------------
+  //      Sensor Selection
+  #define TCS3200 1            //Meroe, Land
+  #define TSL237T 0            //Edwin
+  #define TSL235R 0            //Alpha 2
+  //      Board Version
+  #define MEROE_PCB 1
+  #define LAND_PCB 0
+  #define SONAR_PCB 0
+  #define EDWIN_PCB 0
+  #define ALPHA2_PCB 0
+  //      Camera Options
+  #define S1Logic LOW          //LOW for Alpha shutters and HIGH for Sonar shutters
+  #define ALPHA 1              //1 if ALPHA camera functions
+  #define SONAR 0              //1 for Sonar camera functions   
+  //      Dongle Options 
+  #define UDONGLE 1
   #define ORIGAMI 0
   #define ORIGAMIV1 0
-  #define UDONGLE 1
-  #define GREEN 6 //DONGLE GREEN LED ADRESS
-  #define RED 7   //DONGLE RED LED ADRESS
-  #define DOUBLECLICK 0
-  #define SHUTTERDARKSLIDE 0 // makes you press shutter button to eject darkslide. This is to prevent externally powered cameras from firing the darkslide when opening.
-  #define APERTURE_PRIORITY 0
-  #define COUNTER_BLINK 0
-  #define TIMER_MIRROR_UP 1
-  #define EIGHT_SHOT_PACK 1
-  #define LIGHMETER_HELPER 1
-  #define EJECT_AFTER_DEPRESSING 0 // Ejects the film only after depressing the shutter button. This gives time to the photographer to shield the camera from light.
+  
+  #define GREEN 6
+  #define RED 7
+  //      Feature toggles
+  #define DOUBLECLICK 0        //1 Enables double clicking the shutter button without a dongle to start 10s self timer
+  #define SHUTTERDARKSLIDE 0   //1 Enables feature to not eject dark slide until shutter button is pressed
+  #define APERTURE_PRIORITY 0  //1 Enables SOL2 engage on picture taking for experimental aperture priority mode
+  #define COUNTER_BLINK 0      //1 Switches dongle LED functionality from displaying ISO to displaying the number of shots remaining in your film pack
+  #define TIMER_MIRROR_UP 1    //1 Enables mirror up feature for self timer modes
+  #define EIGHT_SHOT_PACK 1    //1 Makes all counter-based functions work based on an 8 shot pack rather than 10
+  #define LIGHMETER_HELPER 1   //1 Enables viewfinder light meter helper
+  //----------------END CAMERA PCB OPTIONS SELECTION------------------------
+  
   //----------------ISO VALUES VALUES---------------------------------------
   #define ISO_SX70 125
   #define ISO_600 640
