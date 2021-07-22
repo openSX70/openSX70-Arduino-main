8851aaac48ffef2ade9dfbf12bad31836c0c1b2d
zanepollard <pollardzane@gmail.com>
2021-07-22 11:59:20 -0500

    settings additions

--- WORKING/opensx70_lib/settings.h
+++ WORKING/opensx70_lib/settings.h
@@ -26,6 +26,7 @@
   #define SONAR_PCB 0
   #define EDWIN_PCB 0
   #define ALPHA2_PCB 0
+  #define ECM_PCB 0
   //      Camera Options
   #define S1Logic LOW          //LOW for Alpha shutters and HIGH for Sonar shutters
   #define ALPHA 1              //1 if ALPHA camera functions
@@ -34,17 +35,17 @@
   #define UDONGLE 1
   #define ORIGAMI 0
   #define ORIGAMIV1 0
-  
   #define GREEN 6
   #define RED 7
   //      Feature toggles
-  #define DOUBLECLICK 0        //1 Enables double clicking the shutter button without a dongle to start 10s self timer
-  #define SHUTTERDARKSLIDE 0   //1 Enables feature to not eject dark slide until shutter button is pressed
-  #define APERTURE_PRIORITY 0  //1 Enables SOL2 engage on picture taking for experimental aperture priority mode
-  #define COUNTER_BLINK 0      //1 Switches dongle LED functionality from displaying ISO to displaying the number of shots remaining in your film pack
-  #define TIMER_MIRROR_UP 1    //1 Enables mirror up feature for self timer modes
-  #define EIGHT_SHOT_PACK 1    //1 Makes all counter-based functions work based on an 8 shot pack rather than 10
-  #define LIGHMETER_HELPER 1   //1 Enables viewfinder light meter helper
+  #define DOUBLECLICK 0            //1 Enables double clicking the shutter button without a dongle to start 10s self timer
+  #define SHUTTERDARKSLIDE 0       //1 Enables feature to not eject dark slide until shutter button is pressed
+  #define APERTURE_PRIORITY 0      //1 Enables SOL2 engage on picture taking for experimental aperture priority mode
+  #define COUNTER_BLINK 0          //1 Switches dongle LED functionality from displaying ISO to displaying the number of shots remaining in your film pack
+  #define TIMER_MIRROR_UP 1        //1 Enables mirror up feature for self timer modes
+  #define EIGHT_SHOT_PACK 1        //1 Makes all counter-based functions work based on an 8 shot pack rather than 10
+  #define LIGHMETER_HELPER 1       //1 Enables viewfinder light meter helper
+  #define EJECT_AFTER_DEPRESSING 0 //1 Enables the user to hold the shutter button to prevent photo ejection
   //----------------END CAMERA PCB OPTIONS SELECTION------------------------
 
   //----------------ISO VALUES VALUES---------------------------------------
