9e0fbc63072f728134a5bdd59333735ffa8d37d5
zanepollard <pollardzane@gmail.com>
2022-02-20 16:51:44 -0600

    ECM PCB pinouts

--- WORKING/opensx70_lib/sx70_pcb.h
+++ WORKING/opensx70_lib/sx70_pcb.h
@@ -108,6 +108,7 @@
     const uint8_t PIN_LED2 = A3;
 
   #elif ECM_PCB
+    const uint8_t PIN_FPIN = 6;
     const uint8_t PIN_S1 = 12; //Sonar S1T S1 Trigger Redbutton
     const uint8_t PIN_S2 = A4;
     const uint8_t PIN_S3 = 8;
@@ -124,18 +125,19 @@
     const uint8_t PIN_LED1 = 13; //LED RED
     const uint8_t PIN_LED2 = A3; //LED BLUE
     const uint8_t PIN_FF = 4;
+
+    const uint8_t PIN_SOL1LOW = 20; //SOL1 low power on ECM_PCB
+    const uint8_t PIN_SOL2LOW = 21; //SOL2 low power on ECM_PCB
+    const uint8_t PIN_ISO=A6;       //MANUAL    iSO SELECTION
+    const uint8_t PIN_FT = A7;      //Sonar Flash Triggerd INPUT -- Cave Analoge Input only!
+    //const uint8_t TCS3200_S1_Pin = 9;
+    const uint8_t TCS3200_OE_Pin = 9;         //Pin 13 on Meroe2  (PB1-OC1A)
+	      
     #if SONAR
       const uint8_t PIN_GTD = A5; // Sonar gone that distance
       const uint8_t PIN_S1F = 10; //Sonar S1 Focus //Analog imput only
-      const uint8_t PIN_FT = A7; //Sonar Flash Triggerd INPUT -- Cave Analoge Input only!
       const uint8_t PIN_S1F_FBW = 2; //engage SONAR prefocus
-      const uint8_t TCS3200_S1_Pin = 9;
-      const uint8_t TCS3200_S3_Pin = 6;
-    #else
-      const uint8_t PIN_OE = 9;         //Pin 13 on Meroe2  (PB1-OC1A)
-      const uint8_t TCS3200_S1_Pin = 2; //Pin 32 on Meroe2  (PD2-INT0)
-      const uint8_t TCS3200_S3_Pin = 6; //Pin 10 on Meroe2 (PD6-AIN0)
-    #endif
+	  #endif
   #endif
 
   void io_init();
