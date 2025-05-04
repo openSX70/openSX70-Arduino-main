81a4f2b2e460a59099561ce6b225a7a65eec3e14
Zane Pollard <pollardzane@gmail.com>
2025-05-04 12:07:06 -0500

    Settings changes

--- WORKING/opensx70_lib/settings.h
+++ WORKING/opensx70_lib/settings.h
@@ -30,7 +30,7 @@
   #define RED 7
   //      Feature toggles
   #define DOUBLECLICK 0            //1 Enables double clicking the shutter button without a dongle to start 10s self timer
-  #define SHUTTERDARKSLIDE 0       //1 Enables feature to not eject dark slide until shutter button is pressed
+  #define SHUTTERDARKSLIDE 1       //1 Enables feature to not eject dark slide until shutter button is pressed
   #define TIMER_MIRROR_UP 1        //1 Enables mirror up feature for self timer modes
   #define EIGHT_SHOT_PACK 1        //1 Makes all counter-based functions work based on an 8 shot pack rather than 10
   #define LIGHMETER_HELPER 1       //1 Enables viewfinder light meter helper
@@ -41,7 +41,7 @@
   //----------------ISO VALUES VALUES---------------------------------------
   #define ISO_600 640
   #define ISO_SX70 125
-  #define DEFAULT_ISO ISO_SX70
+  #define DEFAULT_ISO ISO_600
   //---------------END ISO VALUES--------------------------------------------
 
   //---------------MAGIC NUMBERS---------------------------------------------
