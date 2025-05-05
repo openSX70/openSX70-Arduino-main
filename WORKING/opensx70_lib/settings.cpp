81e23646ce3f5af5f235719968d26b5c349b3ed8
Zane Pollard <pollardzane@gmail.com>
2025-05-04 19:33:53 -0500

    variance changes and meter helper range changes

--- WORKING/opensx70_lib/settings.cpp
+++ WORKING/opensx70_lib/settings.cpp
@@ -11,7 +11,7 @@ byte lightmeterHelper = true;
 // Added to remove the need to check for selector values prior to picture taking.
 
 //int ShutterSpeed[] = {     25,   29,   32,    34,  39,   44,  64,   64,    175,  311, 609, 1109, POST, POSB, AUTO600, AUTO100 };
-int ShutterSpeed[] = {     25,   29,   32,    34,  39,   44,  64,   64,    175,  311, 609, 1109, POST, POSB, AUTO600, AUTO100 };
+int ShutterSpeed[] = {     25,   29,   31,    34,  39,   44,  64,   120,    175,  311, 609, 1109, POST, POSB, AUTO600, AUTO100 };
 //int ShutterSpeed[] = { 2000, 1000,  500,   250, 125,   60,  30,   15,      8,    4,   2,    1, T, B, AUTO600, AUTO100 };
 //                        0     1      2      3    4      5    6     7       8     9    10   11
 
@@ -19,16 +19,16 @@ int flashDelay = 1; //new flash "system"
   // (These are default if not set, but changeable for convenience)
 
 //int ShutterVariance[] = {  15,   10,   8,     6,    6,    6,   6,    6,      10,    20,   40,   40};
-int ShutterVariance[] = {  15,   10,   8,     6,    6,    6,   6,    6,      10,    20,   40,   40};
+int ShutterVariance[] = {  3,   3,   8,     6,    6,    6,   6,    6,      10,    20,   40,   40};
 //int ShutterSpeed[] = { 2000, 1000,  500,   250,  125,   60,  30,   15,      8,    4,   2,   1
 //                          0     1     2      3    4     5    6     7       8     9    10   11
 
 //int MinRange[] =       {   10,   23,   24,    28,   33,   38,  44,   50,    121,   200, 551,  1001};
-int MinRange[] =       {   10,   23,   24,    28,   33,   38,  44,   50,    121,   200, 551,  1001};
-//int ShutterSpeed[] = { 2000, 1000,  500,   250,  125,   60,  30,   15,      8,    4,   2,   1
+int MinRange[] =       {   20,   26,   28,    32,   35,   40,  45,   65,    121,   176, 312,  610};
+//int ShutterSpeed[] = { 2000, 1000,  500,   250,  125,   60,  30,   15,     8,    4,    2,    1
 //                          0     1     2      3    4     5    6     7       8     9    10   11
 
 //int MaxRange[] =       {   25,   29,   32,    34,   39,   49,  89,   120,    199,  550, 1000, 1250};
-int MaxRange[] =       {   25,   29,   32,    34,   39,   49,  89,   120,    199,  550, 1000, 1250};
+int MaxRange[] =       {   25,   29,   31,    34,   39,   44,  64,   120,    175,  311, 609, 1109};
 //int ShutterSpeed[] = { 2000, 1000,  500,   250,  125,   60,  30,   15,      8,    4,   2,   1
 //                          0     1     2      3    4     5    6     7       8     9    10   11
